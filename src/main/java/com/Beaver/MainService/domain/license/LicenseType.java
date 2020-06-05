df2a8d133bb12e92c8879aabfbf055c3dd8b2fe4
heesujung2 <heesujung@n3n.co.kr>
2020-06-05 19:00:37 +0900

    ADD License Page

--- src/main/java/com/Beaver/MainService/domain/license/LicenseType.java
+++ src/main/java/com/Beaver/MainService/domain/license/LicenseType.java
@@ -2,11 +2,12 @@ package com.Beaver.MainService.domain.license;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
 
 @Getter
 @RequiredArgsConstructor
 public enum LicenseType {
+
+
     PERMANENT("LICENSE_PERMANENT", "영구"),
     ONE_YEAR("LICENSE_ONE_YEAR", "1년"),
     MARKETING("LICENSE_MARKETING", "마케팅"),
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,38 +1,51 @@
-var main = {
+
+var licenseMain = {
+
     init : function () {
         var _this = this;
-        $('btn-license-generate').on('click', function() {
+        $('#btn-license-generate').on('click', function() {
             _this.licenseGenerate();
-
         });
-
     },
 
 
-    licenseGenerate : function {
+    licenseGenerate : function (){
         var data = {
             generateNumber : 10,
-            type: "LICENSE_PERMANENT",
-            product: "BABY_SIGN",
+            type : 0,
+            product: 0,
             licensePeriod : 365
         };
 
         $.ajax({
             type: 'POST',
-            url: '/api/v1/license',
+            url: '/api/v1/license/generate',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data)
+            data: JSON.stringify(data),
             success: function(data) {
+                alert('라이센스 생성 중 입니다.');
+/*
+                var commentList = ""
+                data.find("code").each(function() {
+                    commentList += "<div>";
+
+
+                })
+
                 var result = data;
                 return result;
+                */
             }
         }).done(function() {
-            alert('라이센스 생성중 입니다.');
-        }).fail(function(error)) {
-            alert(JSON.stringify(error))
+            alert("라이센스 생성 완료");
+        }).fail(function(error) {
+            alert("에러 발생" + JSON.stringify(error))
         });
     }
 
 
 }
+
+licenseMain.readyAx5ui();
+licenseMain.init();
